31ddd2a787f8d4dfc06ab1046cda2d713f3aab28
Tomasz Flakus <tomasz.flakus@gmail.com>
2020-03-01 19:38:03 +0100

    adding teplatestring and updating UI content

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,25 @@ const cityForm = document.querySelector("form");
 const card = document.querySelector(".card");
 const details = document.querySelector(".details");
 
+const updateUI = data => {
+  const cityDets = data.cityDets;
+  const weather = data.weather;
+
+  // update details template
+  details.innerHTML = `
+                      <h5 class="my-3">${cityDets.EnglishName}</h5>
+                        <div class="my-3">${weather.WeatherText}</div>
+                        <div class="display-4 my-4">
+                         <span>${weather.Temperature.Metric.Value}</span>
+                         <span>&deg;C</span>
+                         </div>
+  `;
+  // remove d-none class from .card
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
+};
+
 const updateCity = async city => {
   const cityDets = await getCity(city);
   const weather = await getWeather(cityDets.Key);
@@ -22,6 +41,6 @@ cityForm.addEventListener("submit", e => {
 
   //   update the UI with the city
   updateCity(city)
-    .then(data => console.log(data))
+    .then(data => updateUI(data))
     .catch(err => console.log(err));
 });
